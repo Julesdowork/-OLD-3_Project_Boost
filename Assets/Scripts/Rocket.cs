64d1160c1cc5d9b2782a46ea9cf4524835ef4eda
Julian McNeill <julesdowork@gmail.com>
2018-07-05 15:34:34 -0400

    Tagging Game Objects As Friendly

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -20,11 +20,24 @@ public class Rocket : MonoBehaviour {
         Rotate();
     }
 
+    void OnCollisionEnter (Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly": print("Okay");
+                break;
+            case "Fuel": print("Fueling up");
+                break;
+            default: print("Dead");
+                break;
+        }
+    }
+
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))    // can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)     // so it doesn't layer
             {
                 audioSource.Play();
